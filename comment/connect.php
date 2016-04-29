8a1c1199e3ea0c41e4ae35db9fc69908a48eb231
darknamer <darknamer@gmail.com>
2016-04-30 04:59:25 +0700

    update database

--- comment/connect.php
+++ comment/connect.php
@@ -1,9 +1,17 @@
-<?php 
-        $dbhost = "localhost";
-        $dbusername = "root"; // ชื่อผู้ใช้งาน database หากทดสอบในเครื่องตัวเองให้ใช้ root 
-        $dbpassword = "";
-        $dbname = "bakery_online";
-        $dbport = "3306";
-        $conn = mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname);
-        mysqli_set_charset($conn, 'utf8');
-?>
+<?php
+
+$dbhost = "localhost";
+$dbusername = "root"; // ชื่อผู้ใช้งาน database หากทดสอบในเครื่องตัวเองให้ใช้ root
+$dbpassword = "";
+$dbname = "bakery_online";
+$dbport = "3306";
+
+$conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
+mysqli_set_charset($conn, 'utf8');
+
+function debugArray($array) {
+  echo '<pre>';
+  print_r($array);
+  echo '</pre>';
+  die;
+}
--- comment/insert.php
+++ comment/insert.php
@@ -1,23 +1,20 @@
 <?php
 
-require_once("connect.php"); //เรียกใช้ไฟล์การเชื่อมต่อDATABASE SERVERและฐานข้อมูล
+require_once("connect.php"); //เรียกใช้ไฟล์การเชื่อมต่อ DATABASE SERVER และฐานข้อมูล
+
+//debugArray($_POST);
 
 date_default_timezone_set('Asia/Bangkok');
-$datetime_com = date('Y-m-d H:i:s');
-$id_order = $_POST['preorder_detail'];
-$orderno = $_POST['preorder_detail'];
-$id_bakery = $_POST['preorder_detail'];
-$id_sp = $_POST['preorder_detail'];
-$id_type = $_POST['preorder_detail'];
-$com_text = $_POST['com_text'];
+//$datetime_com = date('Y-m-d H:i:s');
+//$id_order = $_POST['preorder_detail'];
+//$orderno = $_POST['preorder_detail'];
+//$id_bakery = $_POST['preorder_detail'];
+//$id_sp = $_POST['preorder_detail'];
+//$id_type = $_POST['preorder_detail'];
+//$com_text = $_POST['com_text'];
 
-$sql1 = "SELECT com_text FROM comment WHERE com_text = '$com_text'";
-$r1 = mysqli_query($conn, $sql1);
-$sql2 = "INSERT INTO comment(id_order,orderno,id_bakery,id_sp,id_type,datetime,com_text)VALUES('$id_order','$orderno','$id_bakery','$id_sp','$id_type','$datetime_com','$com_text')";
-$r2 = mysqli_query($conn, $sql2);
-//echo $sql2;
-echo "<script>alert('บันทึกสำเร็จ')</script>";
-echo "<meta http-equiv ='refresh'content='0;URL=comment.php'>";
+$sql = "INSERT INTO `comment` (`id_order`, `orderno`, `id_bakery`, `id_sp`, `id_type`, `datetime_com`, `com_text`) VALUES ({$_POST['id_order']}, {$_POST['orderno']}, {$_POST['id_bakery']}, {$_POST['id_sp']}, {$_POST['id_type']}, now(), '{$_POST['com_text']}');";
+$conn->query($sql) or die($sql);
 
-mysqli_close($conn);
-?>
+header('Location: comment.php');
+exit();
