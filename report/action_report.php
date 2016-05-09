593fab5a5abc9e4adbeddc22f4373cbb26c12d52
darknamer <darknamer@gmail.com>
2016-05-09 10:10:38 +0700

    script report

---
+++ report/action_report.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: darknamer
+ * Date: 5/9/16
+ * Time: 01:27
+ */
+
+ob_start();
+session_start();
+
+include './connect.php';
+require_once '../library/dompdf/autoload.inc.php';
+
+use Dompdf\Dompdf;
+//use Dompdf\Options;
+
+//$options = new Options();
+//$options->set('defaultFont', 'Helvetica');
+//$domPDF = new Dompdf($options);
+$domPDF = new Dompdf(['defaultFont' => 'Helvetica']);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $start_date = $_POST['startDatePicker'];
+  $end_date = $_POST['endDatePicker'];
+
+  $html = "<html><head><title></title></head><body>";
+  $html .= "<h1>Training DOMPDF</h1>";
+  $html .= "</body></html>";
+
+  switch ($_POST['report']) {
+    // ยอดขายเบเกอรี่
+    case 'sale':
+      break;
+    // เบเกอรี่ขายดี 5 อันดับ
+    case 'bakery':
+
+      break;
+    // การแสดงความคิดเห็นต่อเบเกอรี่
+    case 'comment':
+
+      break;
+    default:
+      break;
+  }
+
+  $domPDF->loadHtml($html);
+  $domPDF->setPaper('A4', 'portrait');
+  $domPDF->stream('report');
+}
--- report/connect.php
+++ report/connect.php
@@ -1,12 +1,18 @@
-<?php   
-    $dbhost = "localhost";
-    $dbusername = "root"; // ชื่อผู้ใช้งาน database หากทดสอบในเครื่องตัวเองให้ใช้ root 
-    $dbpassword = "";
-    $dbname = "bakery_online";
-    $dbport = "3306";
-    
-    $conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname, $dbport)
-            or die("ไม่สามารถติดต่อฐานข้อมูลได้");
+<?php
 
-    mysqli_set_charset($conn, 'utf8');
-?>
+function debugArray($array)
+{
+  echo '<pre>';
+  print_r($array);
+  echo '</pre>';
+  die;
+}
+
+$dbhost = "localhost";
+$dbusername = "root"; // ชื่อผู้ใช้งาน database หากทดสอบในเครื่องตัวเองให้ใช้ root
+$dbpassword = "";
+$dbname = "bakery_online";
+$dbport = "3306";
+
+$conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname, $dbport) or die("ไม่สามารถติดต่อฐานข้อมูลได้");
+mysqli_set_charset($conn, 'utf8');
